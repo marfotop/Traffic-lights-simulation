8dec5ef8cf537d95587aaa9ed0fd39fa2d59ccbe
marfotop <120735186+marfotop@users.noreply.github.com>
2023-03-02 21:00:53 +0200

    Add files via upload

---
+++ atomiki_ergasia_p20201/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace atomiki_ergasia_p20201
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.timer7 = new System.Windows.Forms.Timer(this.components);
+            this.timer8 = new System.Windows.Forms.Timer(this.components);
+            this.timer9 = new System.Windows.Forms.Timer(this.components);
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Location = new System.Drawing.Point(1, 355);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(319, 278);
+            this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::atomiki_ergasia_p20201.Properties.Resources.horizontal_red;
+            this.pictureBox2.Location = new System.Drawing.Point(258, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(61, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 278);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(438, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(319, 278);
+            this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Gray;
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Location = new System.Drawing.Point(438, 355);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(319, 278);
+            this.panel4.TabIndex = 2;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.Location = new System.Drawing.Point(67, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::atomiki_ergasia_p20201.Properties.Resources.vertical_red;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::atomiki_ergasia_p20201.Properties.Resources.horizontal_car3;
+            this.pictureBox8.Location = new System.Drawing.Point(1, 299);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(75, 39);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::atomiki_ergasia_p20201.Properties.Resources.horizontal_car2;
+            this.pictureBox7.Location = new System.Drawing.Point(121, 299);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(75, 39);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::atomiki_ergasia_p20201.Properties.Resources.horizontal_car1;
+            this.pictureBox6.Location = new System.Drawing.Point(245, 299);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(75, 39);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::atomiki_ergasia_p20201.Properties.Resources.car3;
+            this.pictureBox5.Location = new System.Drawing.Point(360, 558);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(39, 75);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::atomiki_ergasia_p20201.Properties.Resources.car2;
+            this.pictureBox4.Location = new System.Drawing.Point(360, 457);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(39, 75);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::atomiki_ergasia_p20201.Properties.Resources.car1;
+            this.pictureBox3.Location = new System.Drawing.Point(360, 355);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(39, 75);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 1;
+            // 
+            // timer5
+            // 
+            this.timer5.Enabled = true;
+            this.timer5.Interval = 1;
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 1;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // timer7
+            // 
+            this.timer7.Enabled = true;
+            this.timer7.Interval = 1;
+            // 
+            // timer8
+            // 
+            this.timer8.Enabled = true;
+            this.timer8.Interval = 1;
+            // 
+            // timer9
+            // 
+            this.timer9.Enabled = true;
+            this.timer9.Interval = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(758, 633);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Timer timer7;
+        private System.Windows.Forms.Timer timer8;
+        private System.Windows.Forms.Timer timer9;
+    }
+}
+
---
+++ atomiki_ergasia_p20201/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atomiki_ergasia_p20201
+{
+    public partial class Form1 : Form
+    {
+        Timer[] timers = new Timer[3];  // timers for vertical cars
+        Timer[] timers2 = new Timer[3]; // timers for horizontal cars
+        PictureBox[] v_cars = new PictureBox[3];  // list of vertical cars
+        PictureBox[] h_cars = new PictureBox[3];  // list of horizontal cars
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            v_cars[0] = pictureBox3;
+            v_cars[1] = pictureBox4;
+            v_cars[2] = pictureBox5;
+            h_cars[0] = pictureBox6;
+            h_cars[1] = pictureBox7;
+            h_cars[2] = pictureBox8;
+            timers[0] = timer3;
+            timers[1] = timer4;
+            timers[2] = timer5;
+            timers2[0] = timer7;
+            timers2[1] = timer8;
+            timers2[2] = timer9;
+        }
+
+        // Drawing the road 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+               Color.DarkGray, 0, ButtonBorderStyle.Solid, // left
+               Color.DarkGray, 0, ButtonBorderStyle.Solid, // top
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid, // right
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid);// bottom
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid, // left
+               Color.DarkGray, 0, ButtonBorderStyle.Solid, // top
+               Color.DarkGray, 0, ButtonBorderStyle.Solid, // right
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid);// bottom
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+               Color.DarkGray, 0, ButtonBorderStyle.Solid, // left
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid, // top
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid, // right
+               Color.DarkGray, 0, ButtonBorderStyle.Solid);// bottom
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, panel1.ClientRectangle,
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid, // left
+               Color.RosyBrown, 23, ButtonBorderStyle.Solid, // top
+               Color.DarkGray, 0, ButtonBorderStyle.Solid, // right
+               Color.DarkGray, 0, ButtonBorderStyle.Solid);// bottom
+        }
+
+        // control of traffic lights
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (count < 2)
+            {
+                pictureBox1.ImageLocation = "vertical_red.png";
+            }
+            else if (count < 8)
+                pictureBox2.ImageLocation = "horizontal_green.png";
+            else if (count < 13)
+            {
+                pictureBox2.ImageLocation = "horizontal_yellow.png";
+            }
+            else if (count < 15)
+            {
+                pictureBox2.ImageLocation = "horizontal_red.png";
+
+            }
+            else if (count < 17)
+                pictureBox1.ImageLocation = "vertical_green.png";
+            else if (count < 19)
+            {
+                pictureBox1.ImageLocation = "vertical_yellow.png";
+            }
+            else
+                count = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer6.Enabled = true;
+        }
+
+        // control of vertical cars
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // green or yellow vertical traffic light
+            if (pictureBox1.ImageLocation == "vertical_green.png" || 
+                pictureBox1.ImageLocation == "vertical_yellow.png")
+            {
+                for (int i=0; i<v_cars.Length; i++)
+                {
+                    if (v_cars[i].Location.Y > 0)
+                        v_cars[i].Location =
+                            new Point(v_cars[i].Location.X, v_cars[i].Location.Y - 1);
+                    else
+                        v_cars[i].Location = new Point(v_cars[i].Location.X, 600);
+                }
+            }
+            else  // red traffic light
+            {
+                for (int i=0; i<v_cars.Length; i++)
+                {
+                    // if car is just behind the red traffic light
+                    if (v_cars[i].Location.Y <= 289 &&
+                        v_cars[i].Location.Y >= 288)
+                    {
+                        v_cars[i].Location = new Point(v_cars[i].Location.X, 288);
+                        timers[i].Enabled = false;
+                    }
+                    // if car is second at the queue of red traffic light and has one car in front of it
+                    else if (i > 0 && v_cars[i].Location.Y <= 371
+                        && v_cars[i].Location.Y >= 370 && v_cars[i-1].Location.Y <=
+                        289 && v_cars[i-1].Location.Y >= 288)
+                    {
+                        v_cars[i].Location = new Point(v_cars[i].Location.X, 370);
+                        timers[i].Enabled = false;
+                    }
+                    // if car is third at the queue of red traffic light and has two cars in front of it
+                    else if (i > 1 && v_cars[i].Location.Y <= 454
+                        && v_cars[i].Location.Y >= 453 && v_cars[i-2].Location.Y <=
+                        289 && v_cars[i-2].Location.Y >= 288)
+                    {
+                        v_cars[i].Location = new Point(v_cars[i].Location.X, 453);
+                        timers[i].Enabled = false;
+                    }
+                    // if the first car is now last at the queue
+                    else if (i==0 && v_cars[i].Location.Y <= 454
+                        && v_cars[i].Location.Y >= 453 && v_cars[i+2].Location.Y <=
+                        371 && v_cars[i+2].Location.Y >= 370)
+                    {
+                        v_cars[i].Location = new Point(v_cars[i].Location.X, 453);
+                        timers[i].Enabled = false;
+                    }
+                    // if car has passed the red traffic light and is in front of it, so it doesn't need to stop
+                    else if (v_cars[i].Location.Y > 0)
+                        v_cars[i].Location =
+                            new Point(v_cars[i].Location.X, v_cars[i].Location.Y - 1);
+                    else
+                        v_cars[i].Location = new Point(v_cars[i].Location.X, 600);
+                }
+            }
+        }
+
+        // control of horizontal cars
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            // green or yellow horizontal traffic light
+            if (pictureBox2.ImageLocation == "horizontal_green.png" ||
+                pictureBox2.ImageLocation == "horizontal_yellow.png")
+            {
+                for (int i=0; i<h_cars.Length; i++)
+                {
+                    if (h_cars[i].Location.X >= -50 && h_cars[i].Location.X < 720)
+                        h_cars[i].Location = new Point(h_cars[i].Location.X + 1, h_cars[i].Location.Y);
+                    else
+                        h_cars[i].Location = new Point(-50, h_cars[i].Location.Y);
+                }
+            }
+            else  // red traffic light
+            {
+                for (int i=0; i<h_cars.Length; i++)
+                {
+                    // if car is just behind the red traffic light
+                    if (h_cars[i].Location.X >= 183 && h_cars[i].Location.X <= 184)
+                    {
+                        h_cars[i].Location = new Point(184, h_cars[i].Location.Y);
+                        timers2[i].Enabled = false;
+                    }
+                    // if car is second at the queue of red traffic light and has one car in front of it
+                    else if (i > 0 && h_cars[i].Location.X >= 90
+                        && h_cars[i].Location.X <= 91 && h_cars[i - 1].Location.X >=
+                        163 && h_cars[i - 1].Location.X <= 184)
+                    {
+                        h_cars[i].Location = new Point(91, h_cars[i].Location.Y);
+                        timers[i].Enabled = false;
+                    }
+                    // if car is third at the queue of red traffic light and has two cars in front of it
+                    else if (i > 1 && h_cars[i].Location.X >= 0
+                        && h_cars[i].Location.X <= 1 && h_cars[i - 2].Location.X >=
+                        183 && h_cars[i - 2].Location.X <= 184)
+                    {
+                        h_cars[i].Location = new Point(1, h_cars[i].Location.Y);
+                        timers[i].Enabled = false;
+                    }
+                    // if the first car is now last at the queue
+                    else if (i==0 && h_cars[i].Location.X >= 0
+                        && h_cars[i].Location.X <= 1 && h_cars[i + 1].Location.X >=
+                        184 && h_cars[i + 1].Location.X <= 185)
+                    {
+                        h_cars[i].Location = new Point(0, h_cars[i].Location.Y);
+                        timers[i].Enabled = false;
+                    }
+                    // if car has passed the red traffic light and is in front of it, so it doesn't need to stop
+                    else
+                    {
+                        if (h_cars[i].Location.X >= -50 && h_cars[i].Location.X < 720)
+                            h_cars[i].Location = new Point(h_cars[i].Location.X + 1, h_cars[i].Location.Y);
+                        else
+                            h_cars[i].Location = new Point(-50, h_cars[i].Location.Y);
+                    }
+                }
+            }
+        }
+    }
+}
